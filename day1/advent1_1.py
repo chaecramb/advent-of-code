744c042ca80c2c89e28e14a6ffaccb043499726b
Chae Cramb <chae.c@simul8.com>
2016-12-01 17:38:14 +0000

    initial solution to part 2 and refactor of part 1

---
+++ day1/advent1_1.py
@@ -0,0 +1,64 @@
+from enum import Enum
+import csv
+
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, direction, distance):
+        if direction == Direction.north:
+            self.y += distance
+        elif direction == Direction.south:
+            self.y -= distance
+        elif direction == Direction.east:
+            self.x += distance
+        elif direction == Direction.west:
+            self.x -= distance
+
+    def coordinates(self):
+        return (self.x, self.y)
+
+
+class Direction(Enum):
+    north = 0
+    east = 1
+    south = 2
+    west = 3
+
+    def turnLeft(self):
+        if self == Direction.north:
+             return Direction(3)
+        return Direction(self.value-1)
+
+    def turnRight(self):
+        if self == Direction.west:
+            return Direction(0)
+        return Direction(self.value+1)
+
+
+def change_direction(current_direction, direction_to_turn):
+    if direction_to_turn == "L":
+        return current_direction.turnLeft()
+    else:
+        return current_direction.turnRight()
+
+
+def distance(path):
+    current_position = Position(0, 0)
+    direction = Direction.north
+    pathway = path.replace(" ", "").split(",")
+
+    for step in pathway:
+        direction_to_turn, distance = step[0], int(step[1:])
+
+        # Change direction        
+        direction = change_direction(direction, direction_to_turn)
+
+        # Modify current_position x and y coords based on distance and direction
+        current_position.move(direction, distance)
+
+    # Final distance calculation
+    x, y = current_position.coordinates()
+    return abs(x) + abs(y)
---
+++ day1/advent1_2.py
@@ -0,0 +1,73 @@
+from enum import Enum
+import csv
+from collections import Counter
+from itertools import cycle
+
+
+class Direction(Enum):   
+    north = 0
+    east = 1
+    south = 2
+    west = 3
+
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.path = [(x, y)]
+        self.current_direction = Direction.north
+
+    def move(self, distance):
+        for i in range(0, distance):
+            self.__adjust_coords()
+            self.path.append(self.coordinates())
+
+    def coordinates(self):
+        return (self.x, self.y)
+
+    def turn(self, direction_to_turn):
+        # TODO: Refactor this ugliness!
+        if direction_to_turn == "L":
+            if self.current_direction == Direction.north:
+                 self.current_direction = Direction(3)
+            else:
+                self.current_direction = Direction(self.current_direction.value-1)
+        else:
+            if self.current_direction == Direction.west:
+                self.current_direction = Direction(0)
+            else:
+                self.current_direction = Direction(self.current_direction.value+1)
+
+    def __adjust_coords(self):
+        if self.current_direction == Direction.north:
+                self.y += 1
+        elif self.current_direction == Direction.south:
+                self.y -= 1
+        elif self.current_direction == Direction.east:
+                self.x += 1
+        elif self.current_direction == Direction.west:
+                self.x -= 1
+
+
+def distance(path):
+    current_position = Position(0, 0)
+    pathway = path.replace(" ", "").split(",")
+
+    for step in pathway:
+        direction_to_turn, distance = step[0], int(step[1:])
+
+        # Change direction        
+        current_position.turn(direction_to_turn)
+
+        # Modify current_position x and y coords based on distance and direction
+        current_position.move(distance)
+
+        # Return early if we've visited a location twice
+        for coord, count in Counter(current_position.path).items():
+            if count > 1:
+                return abs(coord[0]) + abs(coord[1])
+
+    # Final distance calculation    
+    x, y = current_position.coordinates()
+    return abs(x) + abs(y)
