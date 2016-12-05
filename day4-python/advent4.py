279c17b1637876118f600cb116b9bd566418e6b1
Chae Cramb <chaecramb@gmail.com>
2016-12-05 09:03:23 +0000

    initial solution day 4 part 1

--- day4-python/advent4.py
+++ day4-python/advent4.py
@@ -1,2 +1,50 @@
-def real_room(encrypted_name):
-    return True
+from collections import Counter
+
+def parse(encrypted_data):
+    name = encrypted_data[:-11]
+    room_id = int(encrypted_data[-10:-7])
+    checksum = encrypted_data[-6:-1]
+    return (name, room_id, checksum)
+
+
+def count_letters(name):
+    return Counter([c for c in name if c.isalpha()])
+
+
+def most_common(letter_counter, num_letters):
+    """ 
+    Returns a list of the the most common num_letters letter,
+    alphabetised where there are ties
+    """
+    return sorted(letter_counter.items(), key=lambda x: (-x[1], x[0]))[:5]
+
+
+def verify_checksum(letters, checksum):
+    return ''.join(letters) == checksum
+
+
+def real_room(name, checksum):
+    letter_counter = count_letters(name)
+    letters = [l[0] for l in most_common(letter_counter, 5)]
+    checksum_valid = verify_checksum(letters, checksum)
+    return checksum_valid
+
+
+def main(file):
+    sum_of_sectors = 0
+
+    with open(file, 'r') as f:
+        for encrypted_line in f.readlines():
+            name, room_id, checksum = parse(encrypted_line.strip())
+            if real_room(name, checksum):
+                sum_of_sectors += room_id
+
+    return sum_of_sectors
+    print("The sum of the sector IDs of the valid rooms is: " + str(sum_of_sectors))
+
+
+if __name__ == '__main__':
+    sum_of_sectors = main('input.txt')
+    print("The sum of the sector IDs of the valid rooms is: " + str(sum_of_sectors))
+
+
--- day4-python/test_advent4.py
+++ day4-python/test_advent4.py
@@ -1,13 +1,22 @@
 import advent4 as a4
 
-def test1():
-    assert a4.real_room('aaaaa-bbb-z-y-x-123[abxyz]') == True
+def test_encrypted_data_parser1():
+    assert a4.parse('aaaaa-bbb-z-y-x-123[abxyz]') == ('aaaaa-bbb-z-y-x', 123, 'abxyz')
 
-def test2():
-    assert a4.real_room('a-b-c-d-e-f-g-h-987[abcde]') == True
+def test_encrypted_data_parser2():
+    assert a4.parse('nzydfxpc-rclop-qwzhpc-qtylyntyr-769[oshgk]') == ('nzydfxpc-rclop-qwzhpc-qtylyntyr', 769, 'oshgk')
 
-def test3():
-    assert a4.real_room('not-a-real-room-404[oarel]') == False
+def test_valid_room1():
+    assert a4.real_room('aaaaa-bbb-z-y-x', 'abxyz') == True
 
-def test4():
-    assert a4.real_room('totally-real-room-200[decoy]') == False
+def test_valid_room2():
+    assert a4.real_room('a-b-c-d-e-f-g-h', 'abcde') == True
+
+def test_valid_room3():
+    assert a4.real_room('not-a-real-room', 'oarel') == True
+
+def test_fake_room2():
+    assert a4.real_room('totally-real-room', 'decoy') == False
+
+def test_advent4():
+    assert a4.main('test_input.txt') == 1514
