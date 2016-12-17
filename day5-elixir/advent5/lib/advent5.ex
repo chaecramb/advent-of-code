d801468563648d9c72c61cba30d0c252ffcfe525
Chae Cramb <chaecramb@gmail.com>
2016-12-17 09:15:43 +0000

    initial solution day 5

--- day5-elixir/advent5/lib/advent5.ex
+++ day5-elixir/advent5/lib/advent5.ex
@@ -1,6 +1,29 @@
 defmodule Advent5 do
+  def generate_password(door_id) do
+    generate_password(door_id, 0, "")
+  end
 
-  def password(door_id) do
-    "18f47a30"
+  defp generate_password(door_id, index, password) do
+    if String.length(password) == 8 do
+      password
+    else
+      IO.puts(password)
+      IO.puts(index)
+      hash = md5_hash(door_id <> Integer.to_string(index))
+
+      relevant_section = String.slice(hash, 0..5)
+
+      if String.slice(relevant_section, 0..4) == "00000" do
+        next_char = String.last(relevant_section)
+        generate_password(door_id, index + 1, password <> next_char)
+      else 
+        generate_password(door_id, index + 1, password)
+      end
+    end
+  end
+
+  defp md5_hash(input) do
+    :crypto.hash(:md5, input)
+    |> Base.encode16
   end
 end
--- day5-elixir/advent5/test/advent5_test.exs
+++ day5-elixir/advent5/test/advent5_test.exs
@@ -2,7 +2,4 @@ defmodule Advent5Test do
   use ExUnit.Case
   doctest Advent5
 
-  test "the truth" do
-    assert Advent5.password("abc") == "18f47a30"
-  end
 end
