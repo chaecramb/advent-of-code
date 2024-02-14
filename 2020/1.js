16712011b96bb7d3ce2694bb15e577b54400ea91
Chae Cramb <chaecramb@gmail.com>
2024-02-14 07:53:36 +0000

    add a second version of day 1 part 1

--- 2020/1.js
+++ 2020/1.js
@@ -9,11 +9,11 @@ const readFileLines = (filename) =>
 
 const expenses = readFileLines("inputs/1.txt");
 
-const findAddends = (array, sum) => {
+const findAddends = (array, targetSum) => {
   let addends;
   array.forEach((n1) => {
     array.forEach((n2) => {
-      if (n1 + n2 === sum) {
+      if (n1 + n2 === targetSum) {
         addends = [n1, n2];
       }
     });
@@ -23,3 +23,22 @@ const findAddends = (array, sum) => {
 
 const addends = findAddends(expenses, 2020);
 console.log(addends[0] * addends[1]);
+
+const findPairsWithSum = (array, targetSum) => {
+  const pairs = [];
+  const seen = new Set();
+
+  array.forEach((n) => {
+    const complement = targetSum - n;
+    if (seen.has(complement)) {
+      pairs.push([n, complement]);
+    }
+    seen.add(n);
+  });
+
+  return pairs;
+};
+
+const addends2 = findPairsWithSum(expenses, 2020);
+console.log(addends2);
+console.log(addends2[0][0] * addends2[0][1]);
