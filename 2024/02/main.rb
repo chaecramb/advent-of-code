285e9a72b96408f1baec9b85d8861024254a0eb7
Chae Cramb <chaecramb@gmail.com>
2024-12-02 17:44:47 +0000

    Day 2 Part 2 initial solution

--- 2024/02/main.rb
+++ 2024/02/main.rb
@@ -1,13 +1,25 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+reports = ARGF.readlines.map { _1.split.map(&:to_i) }
+
 def safe_report?(report)
   report.each_cons(2).all? { (_1 > _2) && (_1 - _2).abs.between?(1, 3) } ||
   report.each_cons(2).all? { (_1 < _2) && (_1 - _2).abs.between?(1, 3) }
 end
 
-reports = ARGF.readlines.map { _1.split.map(&:to_i) }
 safe_reports = reports.count { safe_report?(_1) }
 
-puts "Safe reports: #{safe_reports}"
+puts "Part 1: Safe reports: #{safe_reports}"
+
+safe_reports = reports.each_with_index.count do |report, i|
+  next true if safe_report?(report)
+
+  (0..report.size).any? do |i|
+    safe_report?(report.reject.with_index { _2 == i })
+  end
+end
+
+puts "Part 2: Safe reports: #{safe_reports}"
+
 
